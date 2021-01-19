b9b74d1de2d8cdaa4b80cab658b4d68a74459dea
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-01-18 16:04:35 -0800

    Url path added

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name="index"),
     # path('/', views.index),
     path('home/', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
