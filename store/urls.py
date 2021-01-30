24c7698f6412e142d8bcf3f6389b2acf88cd48d9
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-01-30 09:57:18 -0800

    Quick url fix

--- store/urls.py
+++ store/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
     path('process_order/', views.processOrder, name="process_order"),
-
 ]
