9905a2413409fae559de67f555e16cf223a2399e
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-01-30 09:55:02 -0800

    Quick url fix

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
+    path('', views.home, name="home"),
     path('shop/', views.shop, name="shop"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
