f70a17775576b1bd3531cb558bd65d6599e73555
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-02-02 12:15:39 -0800

    Redploying changes

--- store/urls.py
+++ store/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('shop/', views.shop, name="shop"),
+    path('', views.shop, name="shop"),
+    path('home/', views.home, name="home"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
