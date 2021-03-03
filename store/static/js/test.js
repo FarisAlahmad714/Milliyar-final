f37bc23355fa0448043333a5088d271b46bc9d00
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-03-03 10:53:35 -0800

    set slider animation with timing

--- store/static/js/test.js
+++ store/static/js/test.js
@@ -1,4 +1,10 @@
-document.getElementById("Clicker").onclick = function () {
-  alert("This is an example of the onclick function");
-  console.log("clicked");
-};
+// document.getElementById("Clicker").onclick = function () {
+//   alert("This is an example of the onclick function");
+//   console.log("clicked");
+// };
+
+function mouseClick() {
+  {
+    document.getElementByClassName("slider-pics").console.log("clicked");
+  }
+}
