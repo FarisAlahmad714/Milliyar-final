44a2d79e22dc60736f36460baafe27699ebc86fe
afnan-1 <roman.nadeem@gmail.com>
2021-10-26 22:55:18 +0500

    test

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -3,11 +3,6 @@
 sudo service nginx reload
 sudo service nginx start
 
-source env/bin/activate
-pip install -r requirements.txt
-python manage.py collectstatic
-python manage.py makemigrations
-python manage.py migrate
 
 
   ApplicationStart:
