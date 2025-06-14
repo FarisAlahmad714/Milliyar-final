6656be9f688e09482cab85d871f0a009d459aa7e
FarisAlahmad714 <Farisalahmad01@hotmail.com>
2025-06-13 20:49:24 -0700

    Replaced browser popup alerts with toast notifications , a little more configuration for production businuess ready ecommerce , recovered this project from 3 years ago

--- EcomProj/settings.py
+++ EcomProj/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = "/static/"
 MEDIA_URL = "/images/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "store/static/images")
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "store/static")]
 
---
+++ build_files.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
